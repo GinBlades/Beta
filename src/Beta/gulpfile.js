df990d42c55225f686ddeb6731a6ea0f47c17ac2
Shaun T Martin <webmaster@ginblades.com>
2016-08-03 17:44:01 -0500

    Working on client resources.

---
+++ src/Beta/gulpfile.js
@@ -0,0 +1,10 @@
+﻿var gulp = require('gulp'),
+    sass = require("gulp-sass");
+
+gulp.task("sass", () => {
+    return gulp.src("./wwwroot/src/**/*.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest("./wwwroot/css"));
+})
+
+gulp.task('default', ["sass"]);
