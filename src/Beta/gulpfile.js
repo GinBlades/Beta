7a8dc07d36b79146c15562d7f150ef070e000656
Shaun T Martin <webmaster@ginblades.com>
2016-08-03 21:00:28 -0500

    Basic assets with gulp

--- src/Beta/gulpfile.js
+++ src/Beta/gulpfile.js
@@ -1,10 +1,36 @@
-﻿var gulp = require('gulp'),
-    sass = require("gulp-sass");
+﻿var gulp = require("gulp"),
+    plumber = require("gulp-plumber"),
+    sass = require("gulp-sass"),
+    maps = require("gulp-sourcemaps"),
+    uglify = require("gulp-uglify"),
+    concat = require("gulp-concat");
 
 gulp.task("sass", () => {
     return gulp.src("./wwwroot/src/**/*.scss")
         .pipe(sass().on("error", sass.logError))
         .pipe(gulp.dest("./wwwroot/css"));
-})
+});
+
+gulp.task("vendorJs", () => {
+    let vendorFiles = [
+        "./wwwroot/lib/jquery/dist/jquery.js",
+        "./wwwroot/lib/bootstrap-sass/assets/javascripts/bootstrap.js"
+    ];
+
+    return gulp.src(vendorFiles)
+        .pipe(plumber())
+        .pipe(maps.init())
+        .pipe(uglify())
+        .pipe(concat("app.js"))
+        .pipe(maps.write("."))
+        .pipe(gulp.dest("./wwwroot/js"));
+});
+
+gulp.task("copyFonts", () => {
+    return gulp.src("./wwwroot/lib/bootstrap-sass/assets/fonts/**/*")
+        .pipe(gulp.dest("./wwwroot/fonts"));
+});
+
+gulp.task("init", ["vendorJs", "copyFonts"]);
 
 gulp.task('default', ["sass"]);
